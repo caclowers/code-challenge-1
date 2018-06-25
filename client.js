ba82bb919aba13c7138e72cc1d66af5b5a07f350
Casey <caclowers@gmail.com>
2018-06-25 14:22:38 -0500

    added swap and delete handlers

--- client.js
+++ client.js
@@ -1,19 +1,37 @@
 console.log('js');
 $(document).ready(readyNow)
 
+let counter = 0
+
 function readyNow() {
     addEventHandler()
 }
 
 function addEventHandler() {
-    $('#genButton').on('click', newDivBlock)
+    $('#genButton').on('click', newDivBlock);
+    $('#swapButton').on('click', toggleColor);
+    $('#deleteButton').on('click', deleteDivBlock)
 }
 
 function newDivBlock() {
     console.log('here');
-    let $div = $('<div class="colorBlock"></div><br>');
+
+    let $div = $('<div id="blockA"></div><br>');
     $div.append('<p id="clickCounter"><span id="countVal">0</span></p>');
     $div.append('<button id="swapButton">SWAP!!!</button>');
     $div.append('<button id="deleteButton">DELETE!!!</button>');
     $('#blockField').append($div);
+    counter++;
+    $('#countVal').text(counter)
+}
+
+function toggleColor() {
+    //get class value of the div
+    if ($('#blockA').hasClass()) {
+        $('#blockA').toggleClass("swapColor")
+    }
+}
+
+function deleteDivBlock() {
+
 }
