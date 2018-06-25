dff3472620f5f02d2d68ca1dfec29d10e4653f35
Casey <caclowers@gmail.com>
2018-06-25 13:57:37 -0500

    add div with color and background, appended to DOM

--- client.js
+++ client.js
@@ -1,7 +1,19 @@
 console.log('js');
 $(document).ready(readyNow)
 
-function readyNow(){
-    console.log('readyNow');
-    
+function readyNow() {
+    addEventHandler()
+}
+
+function addEventHandler() {
+    $('#genButton').on('click', newDivBlock)
+}
+
+function newDivBlock() {
+    console.log('here');
+    let $div = $('<div class="colorBlock"></div><br>');
+    $div.append('<p id="clickCounter"><span id="countVal">0</span></p>');
+    $div.append('<button id="swapButton">SWAP!!!</button>');
+    $div.append('<button id="deleteButton">DELETE!!!</button>');
+    $('#blockField').append($div);
 }
